58a069c0ec1732f8b6fe0fd53d0219159aa8e3c2
APIVerve Team <hello@apiverve.com>
2025-03-28 17:24:42 -0500

    Bug Fixes

--- src/apiverve_horoscope/apiClient.py
+++ src/apiverve_horoscope/apiClient.py
@@ -10,10 +10,10 @@ class HoroscopeAPIClient:
         
         self.api_key = api_key
         self.secure = secure
-        self.base_url = 'https://api.apiverve.com/v1.0.9/horoscope'
+        self.base_url = 'https://api.apiverve.com/v1/horoscope'
         self.headers = {
             'x-api-key': self.api_key,
-            'auth-mode': 'pypi'
+            'auth-mode': 'pypi-package'
         }
 
     def execute(self, params=None):
